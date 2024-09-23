88706cbcd1887299148bf3dcff9c9198cb35a708
Fred <farhadghaher@gamil.com>
2024-09-24 01:27:17 +0330

    add website

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".header_icon")
+let menuBtnIcon = document.querySelector(".header_icon i")
+
+menuBtn.addEventListener('click',function(){
+    
+})
